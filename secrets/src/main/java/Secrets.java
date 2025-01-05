87a6f922ed8485543cae8afde719659e80ee9689
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-01-04 23:47:14 -0400

    Solution to the problem SECRETS

--- secrets/src/main/java/Secrets.java
+++ secrets/src/main/java/Secrets.java
@@ -8,7 +8,7 @@ public class Secrets {
     }
 
     public static int flipBits(int value, int mask) {
-        throw new UnsupportedOperationException(" implement the crs.flipBits() method");
+        throw new UnsupportedOperationException(" implement the crlipBits() method");
     }
 
     public static int clearBits(int value, int mask) {
