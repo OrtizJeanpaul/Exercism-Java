4faea4a95b83b360ca8dd67fd22cce234b7a18c0
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-01-04 23:56:00 -0400

    Solution to the problem SECRETS

--- secrets/src/main/java/Secrets.java
+++ secrets/src/main/java/Secrets.java
@@ -8,7 +8,7 @@ public class Secrets {
     }
 
     public static int flipBits(int value, int mask) {
-        throw new UnsupportedOperationException(" impleme crlipBits() method");
+        throw new UnsupportedOperationException(" impleme crliits() method");
     }
 
     public static int clearBits(int value, int mask) {
