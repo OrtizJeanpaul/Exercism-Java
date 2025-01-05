6496bba2e19b4d332bed81dedebc841ca110d230
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-01-04 23:23:04 -0400

    Solution to the problem SECRETS

--- secrets/src/main/java/Secrets.java
+++ secrets/src/main/java/Secrets.java
@@ -4,7 +4,7 @@ public class Secrets {
     }
 
     public static int setBits(int value, int mask) {
-        throw new UnsupportedOperationException(" implement (static) Secrets.setBits()");
+        throw new UnsupportedOperationException(" implement (static) Secets.setBits()");
     }
 
     public static int flipBits(int value, int mask) {
