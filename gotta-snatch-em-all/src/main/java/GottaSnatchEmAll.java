ec59fe8df347827c91c209ef36c163f96fed2758
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-01-06 12:08:09 -0400

    Solution to the problem JAVA

--- gotta-snatch-em-all/src/main/java/GottaSnatchEmAll.java
+++ gotta-snatch-em-all/src/main/java/GottaSnatchEmAll.java
@@ -4,22 +4,46 @@ import java.util.Set;
 class GottaSnatchEmAll {
 
     static Set<String> newCollection(List<String> cards) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.newCollection() method");
+        Set<String> collection = new HashSet<>();
+
+        for(int i = 0; i < cards.length(); i++){
+            collection.add(cards[i]);
+        }
+        return collection;
     }
 
     static boolean addCard(String card, Set<String> collection) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.addCard() method");
+        return collection.add(card);
     }
 
     static boolean canTrade(Set<String> myCollection, Set<String> theirCollection) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.canTrade() method");
+        Set<String> symmetricDifference = new HashSet<>(myCollection);
+        symmetricDifference.addAll(theirCollection);
+
+        Set<String> intersection = new HashSet<>(myCollection);
+        intersection.retainAll(theirCollection);
+
+        symmetricDifference.removeAll(intersection);
+
+        return symmetricDifference;
     }
 
     static Set<String> commonCards(List<Set<String>> collections) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.commonCards() method");
+        Set<String> dupeCards = new HashSet<>(collections[0]);
+        for(int i = 1; i < collections.length(); i++){
+            dupeCards.retainAll(collections[i]);
+        }
+
+        return dupeCards;
     }
 
     static Set<String> allCards(List<Set<String>> collections) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.allCards() method");
+        Set<String> fullCollection = new HashSet<>();
+
+        for(int i = 0; i < collections.length(); i++){
+            fullCollection.addAll(collections[i]);
+        }
+
+        return fullCollection;
     }
 }
