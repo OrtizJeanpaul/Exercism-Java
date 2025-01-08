e6e0b6acdd284f05f50f4a2f8cffae4adb4307f8
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-01-07 22:38:44 -0400

    Solution to the problem LOGS-LOGS-LOGS

--- logs-logs-logs/src/main/java/LogLine.java
+++ logs-logs-logs/src/main/java/LogLine.java
@@ -1,13 +1,31 @@
 public class LogLine {
-
+    String logLine;
     public LogLine(String logLine) {
+        this.logLine = logLine;
     }
 
     public LogLevel getLogLevel() {
-        throw new UnsupportedOperationException("Please implement the geLogLevel() method");
+        String logSubStr = this.logLine.substring(1,4);
+        switch (logSubStr) {
+            case "TRC":
+                return LogLevel.TRACE;
+            case "DBG":
+                return LogLevel.DEBUG;
+            case "INF":
+                return LogLevel.INFO;
+            case "WRN":
+                return LogLevel.WARNING;
+            case "ERR":
+                return LogLevel.ERROR;
+            case "FTL":
+                return LogLevel.FATAL;
+            default:
+                return LogLevel.UNKNOWN;
+        }
     }
 
     public String getOutputForShortLog() {
-        throw new UnsupportedOperationException("Please implement the getOutputForShortLog() method");
+        String logDescription = this.logLine.substring(5);
+        return this.getLogLevel().getLogType() + logDescription;
     }
 }
